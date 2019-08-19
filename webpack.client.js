52cbe8961aa5e99b0956e2d74a43f9f58312d822
Shourya Mangal <smangal@sgssupport.abudhabi.ae>
2019-08-19 10:20:43 +0400

    GMT webpack fle

--- webpack.client.js
+++ webpack.client.js
@@ -4,6 +4,7 @@ const SRC_DIR = path.join(ROOT_DIR, 'src');
 const BUILD_DIR = path.join(ROOT_DIR, 'build');
 const NODE_MODULES_DIR = path.join(ROOT_DIR, 'node_modules');
 const UILIB_DIR = path.join(NODE_MODULES_DIR, '@tamm');
+const isDebug = false;
 
 module.exports = {
     target: 'node',
@@ -22,23 +23,56 @@ module.exports = {
     symlinks: false,
   },
     module: {
-        rules: [{
-            test: /\.(js|jsx)$/,
-            loader: 'babel-loader',
-            include: [SRC_DIR, UILIB_DIR],
-            // exclude: '/node_modules',
-            options: {
+        rules: [
+            {
+              test:  /\.(js|jsx)$/,
+              include: [SRC_DIR, UILIB_DIR],
+              loader: require.resolve('babel-loader'),
+              options: {
+                babelrc: false,
                 presets: [
-                    'react',
-                    'stage-0',
-                    ['env', {
-                        targets: {
-                            browsers: ['last 2 versions']
-                        }
-                    }]
-                ]
-            }
-        }]
-
+                  [
+                    require.resolve('@babel/preset-env'),
+                    {
+                      targets: {
+                        browsers: ['last 2 versions'],
+                      },
+                      modules: false,
+                    },
+                  ],
+                  require.resolve('@babel/preset-flow'),
+                  require.resolve('@babel/preset-react'),
+                ],
+                plugins: [
+                  [
+                    require.resolve('babel-plugin-import'),
+                    {
+                      libraryName: 'antd',
+                      style: 'css',
+                    },
+                  ],
+                  require.resolve('@babel/plugin-proposal-class-properties'),
+                  require.resolve('@babel/plugin-syntax-dynamic-import'),
+                  ...(isDebug
+                    ? []
+                    : [
+                        require.resolve(
+                          '@babel/plugin-transform-react-constant-elements',
+                        ),
+                        require.resolve(
+                          '@babel/plugin-transform-react-inline-elements',
+                        ),
+                      ]),
+                ],
+                // This is a feature of `babel-loader` for webpack (not Babel itself).
+                // It enables caching results in ./node_modules/.cache/babel-loader/
+                // directory for faster rebuilds.
+                cacheDirectory: true,
+                // Gzipping the cache
+                cacheCompression: !isDebug,
+                compact: !isDebug,
+              },
+            },
+          ],
     }
 }
